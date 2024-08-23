298351a42398ebc82dd11d1e56c90f8d89bbbd72
jahongirabdumalikov <abdumalikovj015@gmail.com>
2024-08-23 06:38:11 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+function addClass(){
+    console.log("efew")
+    const paragraph = document.getElementById('myParagraph');
+    paragraph.classList.add('highlight')
+}
+
+function removeClass(){
+    const paragraph = document.getElementById('myParagraph');
+    paragraph.classList.remove('highlight')
+}
